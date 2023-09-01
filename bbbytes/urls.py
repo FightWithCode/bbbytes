3addda3fcc528036d7aa64672633dad28057deab
FightWithCode <rahulsharmanoj@gmail.com>
2023-09-02 00:36:27 +0530

    changes

--- bbbytes/urls.py
+++ bbbytes/urls.py
@@ -48,5 +48,5 @@ urlpatterns = [
     path('category/<str:slug>/', PagesView.CategoryView, name='CategoryView'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
